080a57aa237e4579a5d77cdfb5abae997eb1bc36
Trung Diec <trungdiec@Trungs-MBP.home>
2021-12-13 13:09:57 -0500

    added values to page

--- script.js
+++ script.js
@@ -6,18 +6,44 @@
 // }
 // fetchInfo();
 
+const beerList = document.querySelector(".beer-list")
 
-
+// function to retreive array of beers
 async function getBeers() {
   let beers = [];
 
-  for (let i = 1; i < 5; i++) {
+  for (let i = 1; i < 2; i++) {
+
     let url = `https://api.punkapi.com/v2/beers?page=${i}&per_page=80`
     let res = await axios.get(`${url}`);
     let currentBeers = res.data;
     beers = [...beers, ...currentBeers];
-    console.log(beers);
+    // console.log(beers)
+    // setBeerValues(beers);
+    convertBeers(beers);
   }
 }
 getBeers();
+
+// convert from object
+function convertBeers(beer) {
+  for (let a = 0; a < beer.length; a++) {
+    // let newBeers = [];
+    // console.log(beer[a].name)
+    let beers = beer[a].name;
+    // console.log(beers);
+    setBeerValues(beers);
+  }
+}
+
+
+//function to set value of beer to DOM
+function setBeerValues(beers) {
+  // beers.forEach(beer => {
+  let option = document.createElement("option");
+  option.value = beers;
+  option.innerHTML = beers;
+  beerList.appendChild(option);
+}
+
 
